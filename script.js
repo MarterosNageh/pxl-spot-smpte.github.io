b3e80616403e0af04b0cd396b18c56418b2261b9
MarterosNageh <146829163+MarterosNageh@users.noreply.github.com>
2023-11-15 19:40:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+// Element 
+let duration = document.getElementById("duration");
+let spot = [document.getElementById("prec25"),document.getElementById("prec50"),document.getElementById("prec75")];
+let float = document.getElementById("float");
+
+
+// Listner 
+
+duration.addEventListener("change",()=>{
+    let runtime = duration.value;
+    if (runtime == ""){
+        runtime = "00:00:00:00";
+    }else{
+    timecodeSpilt(runtime);
+    }
+});
+
+float.addEventListener("click",async()=>{
+    var PnP = await documentPictureInPicture.requestWindow();
+    PnP.document.body.innerHTML = document.body.innerHTML;
+    PnP.document.head.innerHTML = document.head.innerHTML;
+});
+
+
+
+// Function Timecode Precentage 
+function timecodeSpilt(runtime){
+    let hour;
+    let min;
+    let precStart=[];
+    let precEnd=[];
+
+    let h=0;
+    let m=0;
+
+    let runtimeSpilt = runtime.split(":");
+    hour = parseInt(runtimeSpilt[0]);
+    min = parseInt(runtimeSpilt[1]);
+
+    let total = (hour*60) + min;
+    for(let i =0;i<3;i++){
+        precStart[i]= Math.ceil(total *(25*(i+1)/100))-1;
+
+        let condition = true;
+        do {
+            if(precStart[i] >=60){
+                h=+1;
+                precStart[i]= precStart[i]-60;
+            }else{
+                m= precStart[i];
+                condition =false;
+                break;
+            }
+        } while(condition);
+        h= h.toString();
+        m=m.toString();
+        if(h.length === 1){
+            h = "0"+h;
+        }if(m.length ===1){
+            m="0"+m;
+        }
+        spot[i].innerText= h+":"+m+":00:00";
+    }
+
+}
